6dc1e196ece27372fb19eecbbf845cf08ac7e2eb
JinsuKim <jinsu6688@gmail.com>
2019-10-07 13:07:22 +0900

    HashMap 생성, FileReader클래스 사용했습니다.

--- Test.java
+++ Test.java
@@ -1,33 +1,27 @@
-
+import java.util.*;
+import java.io.*;
 /**
  * 여기에 Test 클래스 설명을 작성하십시오.
  * 
- * @author (작성자 이름) 
- * @version (버전번호나 날짜)
+ * @author (2014671038 김진수, 2017605038 스피겔 크릴) 
+ * @version (2014671038 김진수)
  */
 public class Test
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * Test 클래스의 객체 생성자
-     */
-    public Test()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    public static void main(String[] args){
+        int c = 0;
+        HashMap<String,Integer> h = new HashMap<String,Integer>();
+        
+        try{
+            Scanner sc = new Scanner(new FileReader(new File("c:\\Temp\\inputData20191007.txt")));
+            while(sc.hasNext()){
+                String word = sc.nextLine();
+                System.out.println(word);
+            }
+        }catch(IOException e){
+            System.out.println("시스템 오류입니다. ");
+        }
+        
+        
     }
 }
